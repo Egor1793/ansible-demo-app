b7ec222dbba63cdfc9f5b1a2a96cdf60e01a9e1b
Egor1793 <egor.fill.2005filimonov@yandex.ru>
2025-04-01 22:37:48 +0300

    test

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello from Ansible + Alt Linuxi!"
+
+if __name__ == '__main__':
+    app.run(host='192.168.1.1', port=5000)
